fc74833d05881b6f119980d4000c166750d1e8be
developer-soc-sp <developer.soc.sp@gmail.com>
2019-01-22 14:20:40 +0800

    test

--- index.js
+++ index.js
@@ -2,37 +2,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const functions = require('firebase-functions');
-const {WebhookClient,Card, Suggestion} = require('dialogflow-fulfillment');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+var admin = require("firebase-admin");
+const {
+    dialogflow,
+    Image,
+    Table,
+    Carousel,
+   } = require('actions-on-google');
 
 const app = dialogflow(); 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+ 
+app.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
  
   function welcome(agent) {
-     agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-     agent.add(new Card({
-         title: `Title: this is a card title`,
-         imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-         buttonText: 'This is a button',
-         buttonUrl: 'https://assistant.google.com/'
-       })
-     );
+    agent.add(`Welcome to my agent!`);
   }
  
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
   }
-  
+
+  function test(agent) {
+    agent.add(`This is a test`);
+
+  }
+
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('TestIntent',test);
   agent.handleRequest(intentMap);
 });
 
@@ -40,10 +46,10 @@ const expressApp = express().use(bodyParser.json());
 
 expressApp.post('/fulfillment', app.dialogflowFirebaseFulfillment);
 //expressApp.listen(3000);
-var listener = expressApp.listen(process.env.PORT, process.env.IP,
+var listener = expressApp.listen(process.env.PORT,
+  process.env.IP,
   function(){
-  console.log("server started");
-  console.log("listening on port " +
-  listener.address().port);
-  }
-);
+      console.log("server started");
+      console.log("listening on port " +
+      listener.address().port);
+  });
