4d1c3d2b81f56f95645cd26453920123f6849be2
developer-soc-sp <developer.soc.sp@gmail.com>
2019-01-22 14:05:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const functions = require('firebase-functions');
+const {WebhookClient,Card, Suggestion} = require('dialogflow-fulfillment');
+
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+     agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+     agent.add(new Card({
+         title: `Title: this is a card title`,
+         imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+         text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+         buttonText: 'This is a button',
+         buttonUrl: 'https://assistant.google.com/'
+       })
+     );
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  
+ 
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  agent.handleRequest(intentMap);
+});
